cba46e6242dc83431bf7f1ce918e4dcd4ddf070f
Data_Scientist/Trainer <156187339+DS-Python-programmers-Spectrum@users.noreply.github.com>
2026-04-19 22:12:08 -0700

    Add files via upload

---
+++ datascience_internship/fun.py
@@ -0,0 +1,11 @@
+# def add(a,b):
+#     c=a+b
+#     print("sum",c)
+
+# add(a=6,b=10)
+
+def display_info(first_name, last_name):
+    print('First Name:', first_name)
+    print('Last Name:', last_name)
+
+display_info(last_name = 'Cartman')
---
+++ datascience_internship/varr.py
@@ -0,0 +1,57 @@
+# name1="jiji"
+# print(name1)
+
+
+# print(type(name1))
+
+# var1="46789"
+
+
+# var2=float(var1)
+# print(var2)
+
+
+
+# num1=int(input('enter a number'))
+
+# if num1>0:
+#     print("positive")
+
+# elif num1==0:
+#     print("zero")
+# else:
+#     print("negative")
+
+
+
+# for i in range(1,51,3):
+#     print(i)
+
+
+# list1=["apple","cherry","Mango"]
+
+# print(list1[0:2])
+
+
+# year=int(input("Entyer a year:"))
+
+# if(year%4==0 and year%100!=0) or(year %400==0):
+#     print("It is a leap year")
+# else:
+#     print("It is not a leap year")
+
+
+
+
+# num = int(input("Enter the  number"))
+# if num%5 ==0 and num%11 ==0:
+#     print ("it is divisible by 5 and 11")
+# else:
+#     print("not divisible by 5 and 11" )
+# for i in range(3,31,3):
+#     print(i)
+#     multiples=[i for i in range(3,31,3)]
+#     print(multiples )
+
+
+
